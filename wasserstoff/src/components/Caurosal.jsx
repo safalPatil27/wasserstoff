65164f3d9c9d2cfa979562845c9c8b8502a69d55
safalPatil27 <158482783+safalPatil27@users.noreply.github.com>
2024-04-11 13:01:14 +0530

    push

--- wasserstoff/src/components/Caurosal.jsx
+++ wasserstoff/src/components/Caurosal.jsx
@@ -1,9 +1,19 @@
+import React from 'react'
+import courosal1 from './images/caurosal1.png'
+import courosal2 from './images/caurosal2.png'
+import courosal3 from './images/caurosal3.png'
+import courosal4 from './images/caurosal4.png'
 
-
-const Caurosal = () => {
+export default function Caurosal() {
   return (
-    <div>Caurosal</div>
+    
+    <div className='absolute ml-[30%] flex gap-3 overflow-auto no-scrollbar '>
+
+      <img src={courosal1} alt="" />
+      <img src={courosal2} alt="" />
+      <img src={courosal3} alt="" />
+      <img src={courosal4} alt="" />
+     
+    </div>
   )
 }
-
-export default Caurosal
